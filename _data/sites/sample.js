a15a008bb798a963204e536c74fc3b407686090e
mortendk <morten@thememachine.io>
2022-08-05 14:57:04 +0200

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,8 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://www.speedlify.dev/"
-	]
+  name: "Sample", // optional, falls back to object key
+  description: "The default sites that get tested",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: ["https://saga11.dev/"],
 };
