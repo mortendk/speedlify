5eb6b10d9f712f320d58002583b76f6fa0af5b68
mortendk <morten@thememachine.io>
2022-08-05 15:40:50 +0200

    Update thememachine.js

--- _data/sites/thememachine.js
+++ _data/sites/thememachine.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "theme machine", // optional, falls back to object key
   description: "Theme Machine websites",
   options: {
-    // frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 60 * 23, // (in minutes), 23 hours
   },
   urls: [
     "https://thememachine.io/",
