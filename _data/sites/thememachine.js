7398c49535ef84b8a14716f407cdf067abca80bc
mortendk <morten@thememachine.io>
2022-08-05 15:19:03 +0200

    sites

---
+++ _data/sites/thememachine.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "theme machine", // optional, falls back to object key
+  description: "Theme Machine websites",
+  options: {
+    // frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    "https://thememachine.io/",
+    "https://saga11.dev/",
+    "https://morten.dk/",
+    "https://colorfultattoocon.com/",
+  ],
+};
