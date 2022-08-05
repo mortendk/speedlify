81e61124c9739cafbc8f75cac55295fd442829a5
mortendk <morten@thememachine.io>
2022-08-05 16:12:37 +0200

    Create clients.js

---
+++ _data/sites/clients.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "Clients", // optional, falls back to object key
+  description: "Clients",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: ["https://twentythree.com"],
+};
